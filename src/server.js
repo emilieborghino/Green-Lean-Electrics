766548ec53d84f1da24fa4627e77f4f449eea77f
Abrikot <cirque.m@hotmail.fr>
2019-11-07 16:11:29 +0100

    First electricity consumption model

--- src/server.js
+++ src/server.js
@@ -3,35 +3,42 @@ const url = require('url');
 
 const port = 8080;
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
     const reqUrl = url.parse(req.url);
     const service = require('./services.js');
 
-	// GET Endpoint
+    // GET Endpoint
     if (req.method === 'GET') {
         console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
 
-        if(reqUrl.pathname == '/getWindSpeed'){
-	        writeReply(service.getWindSpeed(new Date()), res);
-	    }
+        if (reqUrl.pathname == '/getWindSpeed') {
+            writeReply(service.getWindSpeed(new Date()), res);
+        }
 
-        if(reqUrl.pathname == '/getElectricityConsumption'){
-	        writeReply(service.getElectricityConsumption(new Date()), res);
-	    }
+        if (reqUrl.pathname == '/getElectricityConsumption') {
+            // let sum = 0;
+            // for (let i = 0; i < 3600 * 24; i++) {
+            //     const date = new Date(2019, 10, 7, 0, 0, i);
+            //     // console.log(date, service.getElectricityConsumption(date));
+            //     sum += service.getElectricityConsumption(date).electricityConsumption;
+            // }
+            // console.log("Total : ", sum);
+            writeReply(service.getElectricityConsumption(new Date()), res);
+        }
 
-        if(reqUrl.pathname == '/getCurrentElectricityPrice'){
-	        writeReply(
-	        	service.getCurrentElectricityPrice(
-		        	service.getWindSpeed(new Date()),
-		        	service.getElectricityConsumption(new Date())
-	        	), res);
-	    }
-    } 
+        if (reqUrl.pathname == '/getCurrentElectricityPrice') {
+            writeReply(
+                service.getCurrentElectricityPrice(
+                    service.getWindSpeed(new Date()),
+                    service.getElectricityConsumption(new Date())
+                ), res);
+        }
+    }
 });
 server.listen(port);
 
-function writeReply(response, res){
-	res.statusCode = 200;
+function writeReply(response, res) {
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(response));
 }
--- src/services.js
+++ src/services.js
@@ -6,8 +6,43 @@ exports.getWindSpeed = function (date) {
 
 
 exports.getElectricityConsumption = function (date) {
+    const people = 10;
+    const dailyConsumptionPerPerson = 27;
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const currentTimestamp = hours * 3600 + minutes * 60 + seconds;
+
+    const morningTopTimestamp = 11 * 3600;
+    const afternoonTopHourTimestamp = 19 * 3600 + 30 * 60;
+
+    const morningConsumption = 21;
+    const afternoonConsumption = dailyConsumptionPerPerson - morningConsumption;
+
+    const gaussianFunction = (expectedValue, standardValue, x) => (
+        1.0 / (standardValue * Math.sqrt(2 * Math.PI))
+        * Math.exp(
+            -Math.pow(x - expectedValue, 2)
+            / (2 * Math.pow(standardValue, 2))
+        )
+    );
+
     return {
-        "electricityConsumption": 42
+        "electricityConsumption":
+            (
+                gaussianFunction(
+                    morningTopTimestamp,
+                    21600,
+                    currentTimestamp
+                ) * morningConsumption
+                + gaussianFunction(
+                    afternoonTopHourTimestamp,
+                    5400,
+                    currentTimestamp
+                ) * afternoonConsumption
+            ) * people
     };
 };
 
