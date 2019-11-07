2847efa8b363c0e5c927a7d778a23d0315252f1f
Emilie Borghino <emilie.borghino@neuf.fr>
2019-11-07 14:39:27 +0100

    services file init with the three first web services for the simulator

--- src/server.js
+++ src/server.js
@@ -4,8 +4,34 @@ const url = require('url');
 const port = 8080;
 
 const server = http.createServer(function(req, res) {
-    const page = url.parse(req.url).path;
-    res.writeHead(200);
-    res.end(`You're now connected to '${page}'`);
+    const reqUrl = url.parse(req.url);
+    const service = require('./services.js');
+
+	// GET Endpoint
+    if (req.method === 'GET') {
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
+
+        if(reqUrl.pathname == '/getWindSpeed'){
+	        writeReply(service.getWindSpeed(new Date()), res);
+	    }
+
+        if(reqUrl.pathname == '/getElectricityConsumption'){
+	        writeReply(service.getElectricityConsumption(new Date()), res);
+	    }
+
+        if(reqUrl.pathname == '/getCurrentElectricityPrice'){
+	        writeReply(
+	        	service.getCurrentElectricityPrice(
+		        	service.getWindSpeed(new Date()),
+		        	service.getElectricityConsumption(new Date())
+	        	), res);
+	    }
+    } 
 });
 server.listen(port);
+
+function writeReply(response, res){
+	res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+}
---
+++ src/services.js
@@ -0,0 +1,29 @@
+
+exports.getWindSpeed = function (date) {
+
+    const response = {
+        "windSpeed": 42
+    };
+
+    return response;
+};
+
+
+exports.getElectricityConsumption = function (date) {
+
+    const response = {
+        "electricityConsumption": 42
+    };
+
+    return response;
+};
+
+
+exports.getCurrentElectricityPrice = function (windSpeed, electricityConsumption) {
+
+    const response = {
+        "currentElectricityPrice": 42
+    };
+
+    return response;
+};
