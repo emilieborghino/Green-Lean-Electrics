75da5395a952b6e5e32502d3ce9165d0b1b7a8a9
Abrikot <cirque.m@hotmail.fr>
2019-11-09 19:15:24 +0100

    Fix wind speed over a year

--- src/server.js
+++ src/server.js
@@ -3,6 +3,15 @@ const url = require('url');
 
 const port = 8080;
 
+const routes = {
+    '/getWindSpeed': (service) => service.getWindSpeed(new Date()),
+    '/getElectricityConsumption': (service) => service.getElectricityConsumption(new Date()),
+    '/getCurrentElectricityPrice': (service) => service.getCurrentElectricityPrice(
+        service.getWindSpeed(new Date()),
+        service.getElectricityConsumption(new Date())
+    )
+};
+
 const server = http.createServer(function (req, res) {
     const reqUrl = url.parse(req.url);
     const service = require('./services.js');
@@ -11,34 +20,19 @@ const server = http.createServer(function (req, res) {
     if (req.method === 'GET') {
         console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
 
-        if (reqUrl.pathname == '/getWindSpeed') {
-            writeReply(service.getWindSpeed(new Date()), res);
-        }
-
-        if (reqUrl.pathname == '/getElectricityConsumption') {
-            // let sum = 0;
-            // for (let i = 0; i < 3600 * 24; i++) {
-            //     const date = new Date(2019, 10, 7, 0, 0, i);
-            //     // console.log(date, service.getElectricityConsumption(date));
-            //     sum += service.getElectricityConsumption(date).electricityConsumption;
-            // }
-            // console.log("Total : ", sum);
-            writeReply(service.getElectricityConsumption(new Date()), res);
-        }
-
-        if (reqUrl.pathname == '/getCurrentElectricityPrice') {
-            writeReply(
-                service.getCurrentElectricityPrice(
-                    service.getWindSpeed(new Date()),
-                    service.getElectricityConsumption(new Date())
-                ), res);
+        const route = routes[reqUrl.pathname];
+        if (route) {
+            writeReply(route(service), res);
+        } else {
+            // TODO: add an error message
         }
     }
 });
-server.listen(port);
 
 function writeReply(response, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(response));
 }
+
+server.listen(port);
